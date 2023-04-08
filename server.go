7b8b7ef931245c38cc87b41f90e781f61339d7a2
manuno-design <kidnessfree@outlook.jp>
2023-04-09 03:45:49 +0900

    init commit

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!")
+	})
+	port := os.Getenv("PORT")
+	e.Logger.Fatal(e.Start(port))
+}
